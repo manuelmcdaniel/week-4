7bd2822fc6852ee7950589d170ea66c099009edf
manuelmcdaniel <97697770+manuelmcdaniel@users.noreply.github.com>
2022-04-10 23:14:56 -0400

    Add files via upload

---
+++ rweek 4;4.R
@@ -0,0 +1,71 @@
+#Week 4: dplyr package
+
+
+
+#Task: Write the function to get a dataset from Base R: HairEyeColor
+#and give the dataset a new name of your choice
+show("HairEyeColor")
+america<-data.frame(HairEyeColor)
+
+america
+#See the top rows of the data
+#TASK: Write the function to see the top rows of the data
+
+head(america)
+#Install and call the package dplyr
+#TASK: Write the functions to install and call dplyr
+install.packages("dplyr")
+library(dplyr)
+
+
+#Let's just see the hair and sex columns
+#Task: Write the function to 'select' just the hair and sex columns 
+#(hint: use the 'select' function)
+select(america,Hair,Sex)
+
+#Let's name the dataset with just the two columns, Hair and Sex
+#TASK: Write the function to save the two columns as a new dataset
+#and give it a name
+america1<-select(america,Hair,Sex)
+
+#Let's get rid of the Sex column in the new dataset created above
+#TASK: Write the function that deselects the sex column
+#(hint: use the 'select' function to not select a -column)
+select(america1,Hair)
+
+#Let's rename a column name
+#TASK: Write the function that renames 'sex' to 'gender'
+rename(america,gender=Sex)
+
+#Let's make a new dataset with the new column name
+#TASK: Write the function that names a new dataset that includes the 'gender' column
+AMG<-select(america,Hair,gender=Sex)
+select(AMG,Hair,gender)
+
+#Let's 'filter' just the males from our dataset
+#TASK: Write the function that includes only rows that are 'male'
+filter(AMG,gender == 'Male')
+AMG1<-filter(AMG,gender == 'Male')
+#Let's 'group' our data by gender
+#TASK: Write the function to group the data by gender (hint: group_by)
+group_by(AMG,gender)
+
+#Let's see how many students were examined in the dataset (total the frequency)
+#TASK: replace 'datasetname' with the name of your dataset and get the total
+#After you run it, write the total here:_592__
+total=mutate(america),total=cumsum(Freq))
+#WHEN THE INPUT WAS DONE CODE DOES NOT SHOW THE FREQUENCY
+#THEREFORE BY ASSUPTION USING THE DATA  THE FREQUENCY IS 592
+
+#Since we have a males dataset, let's make a females dataset
+#TASK: Write the function that includes only rows that are 'female'
+filter(AMG,gender == 'Female')
+AMG2<-filter(AMG,gender == 'Female')
+#And now let's join the males and females
+#TASK: Write the function that joins the male and female rows 
+#(hint: try using 'union' or 'bind_rows')
+bind_rows(AMG1,AMG2)
+union(AMG1,AMG2)
+#Optional Task: add any of the other functions 
+#you learned about from the dplyr package
+
